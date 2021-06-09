6361eb9f331aed51786b1c6b492ec138656589a0
Carlo Miras <mirascarlo934@gmail.com>
2021-06-09 16:37:58 +0800

    Change CCP2 to CCP1

--- lab08/lab08-project-mplab.X/main.c
+++ lab08/lab08-project-mplab.X/main.c
@@ -11,7 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <xc-pic32m.h>
+#include <xc.h>
 #include <sys/attribs.h>
 #include <pic32m_builtins.h>
 
@@ -41,7 +41,7 @@ int main(int argc, char** argv) {
     TRISBbits.TRISB0 = 0;
     
     while(1) {
-        delay_ms(1);
+        delay_ms(5);
         LATBbits.LATB0 = out;
         out = !out;
     }
@@ -55,38 +55,67 @@ void setup_pins() {
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
     RPCONbits.IOLOCK = 1;
+    RPOR2bits.RP9R = 6;
     SYSKEY = 0x00000000;
     __builtin_enable_interrupts();
     
     // remap PWM signal to RP9
-    RPOR2bits.RP9R = 6;
 }
 
 void setup_oc() {
-    // Set MCCP operating mode
-    CCP2CON1bits.CCSEL = 0;     // Set MCCP operating mode (OC mode)
-    CCP2CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
+    // Set SCCP operating mode
+    CCP1CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
+    CCP1CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
     
     // Configure SCCP Timebase
-    CCP2CON1bits.T32 = 0;
-    CCP2CON1bits.TMRSYNC = 0;
-    CCP2CON1bits.CLKSEL = 0b000;
-    CCP2CON1bits.TMRPS = 0b00;
-    CCP2CON1bits.TRIGEN = 0;
-    CCP2CON1bits.SYNC = 0b00000;
+    CCP1CON1bits.T32 = 0;
+    CCP1CON1bits.TMRSYNC = 0;
+    CCP1CON1bits.CLKSEL = 0b000;
+    CCP1CON1bits.TMRPS = 0b00;
+    CCP1CON1bits.TRIGEN = 0;
+    CCP1CON1bits.SYNC = 0b00000;
     
     // Configure MCCP output for PWM signal
-    CCP2CON2bits.OCAEN = 1;
-    CCP2CON3bits.POLACE = 0;
-    CCP2TMRbits.TMRL = 0x0000;
+    CCP1CON2bits.OCAEN = 1;
+    CCP1CON3bits.OUTM = 0b000;
+    CCP1CON3bits.POLACE = 0;
+    CCP1TMRbits.TMRL = 0x0000;
     
     // SN: 2017-00076
     // Freq: 20kHz
     // Duty: 76%
-    CCP2PR = 400;               // 8MHz / 20kHz
-    CCP2RA = 0;                 // edge-aligned PWM
-    CCP2RB = 304;               // 400 * 0.76
+    CCP1PRbits.PRL = 400;               // 8MHz / 20kHz
+    CCP1RA = 0;                 // edge-aligned PWM
+    CCP1RB = 304;               // 400 * 0.76
         
-    CCP2CON1bits.ON = 1;
+    CCP1CON1bits.ON = 1;
 }
+
+//void setup_oc() {
+//    // Set SCCP operating mode
+//    CCP2CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
+//    CCP2CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
+//    
+//    // Configure SCCP Timebase
+//    CCP2CON1bits.T32 = 0;
+//    CCP2CON1bits.TMRSYNC = 0;
+//    CCP2CON1bits.CLKSEL = 0b000;
+//    CCP2CON1bits.TMRPS = 0b00;
+//    CCP2CON1bits.TRIGEN = 0;
+//    CCP2CON1bits.SYNC = 0b00000;
+//    
+//    // Configure MCCP output for PWM signal
+//    CCP2CON2bits.OCAEN = 1;
+//    CCP2CON3bits.POLACE = 0;
+//    CCP2TMRbits.TMRL = 0x0000;
+//    
+//    // SN: 2017-00076
+//    // Freq: 20kHz
+//    // Duty: 76%
+//    CCP2PR = 400;               // 8MHz / 20kHz
+//    CCP2RA = 0;                 // edge-aligned PWM
+//    CCP2RB = 304;               // 400 * 0.76
+//        
+//    CCP2CON1bits.ON = 1;
+//}
 
--- lab08/lab08-project-mplab.X/nbproject/Makefile-default.mk
+++ lab08/lab08-project-mplab.X/nbproject/Makefile-default.mk
@@ -19,7 +19,7 @@ endif
 endif
 
 # Environment
-MKDIR=mkdir -p
+MKDIR=gnumkdir -p
 RM=rm -f 
 MV=mv 
 CP=cp 
@@ -70,7 +70,6 @@ OBJECTFILES=${OBJECTDIR}/main.o
 SOURCEFILES=main.c
 
 
-
 CFLAGS=
 ASFLAGS=
 LDLIBSOPTIONS=
@@ -107,18 +106,18 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/main.o: main.c  .generated_files/35eac73bf486f0d6f7c2e146bc481ad78e0b18bc.flag .generated_files/c06a9b750d41494405c7520a01d615c7b830d7c1.flag
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
 else
-${OBJECTDIR}/main.o: main.c  .generated_files/bf67dba3ae21d5c0d1006e2667a9861a067e1da5.flag .generated_files/c06a9b750d41494405c7520a01d615c7b830d7c1.flag
+${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.o.d 
 	@${RM} ${OBJECTDIR}/main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MP -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	@${FIXDEPS} "${OBJECTDIR}/main.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/main.o.d" -o ${OBJECTDIR}/main.o main.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
 endif
 
@@ -133,13 +132,13 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_SIMULATOR=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -mdfp="${DFP_DIR}"
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_SIMULATOR=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -mdfp="${DFP_DIR}"
-	${MP_CC_DIR}/xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
+	${MP_CC_DIR}\\xc32-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/lab08-project-mplab.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} 
 endif
 
 
@@ -158,7 +157,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-DEPFILES=$(shell "${PATH_TO_IDE_BIN}"mplabwildcard ${POSSIBLE_DEPFILES})
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
 ifneq (${DEPFILES},)
 include ${DEPFILES}
 endif
