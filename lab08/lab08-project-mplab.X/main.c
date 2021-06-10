c6b7038d35b49c443c45eb11d4d641b1718d051d
Carlo Miras <mirascarlo934@gmail.com>
2021-06-11 00:17:10 +0800

    Commit

--- lab08/lab08-project-mplab.X/main.c
+++ lab08/lab08-project-mplab.X/main.c
@@ -20,20 +20,23 @@
 void setup_pins();
 void setup_oc();
 
-void delay_ms(int ms) {
+void delay_875ns(int mul) {
     int i = 0;
-    int delay = 1143 * ms;
-    while (i < delay) {
+    while (i < mul) {
          ++i; // 875ns delay
     }
+}
+
+void delay_ms(int ms) {
+    delay_875ns(1143*ms);
 }
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    setup_pins();
     setup_oc();
+    setup_pins();
     
     // testing
     int out = 0;
@@ -41,7 +44,7 @@ int main(int argc, char** argv) {
     TRISBbits.TRISB0 = 0;
     
     while(1) {
-        delay_ms(5);
+        delay_875ns(10);
         LATBbits.LATB0 = out;
         out = !out;
     }
@@ -55,67 +58,65 @@ void setup_pins() {
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
     RPCONbits.IOLOCK = 1;
-    RPOR2bits.RP9R = 6;
+    RPOR4bits.RP19R = 6;     // remap PWM signal to RP19
     SYSKEY = 0x00000000;
     __builtin_enable_interrupts();
-    
-    // remap PWM signal to RP9
-}
-
-void setup_oc() {
-    // Set SCCP operating mode
-    CCP1CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
-    CCP1CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
-    
-    // Configure SCCP Timebase
-    CCP1CON1bits.T32 = 0;
-    CCP1CON1bits.TMRSYNC = 0;
-    CCP1CON1bits.CLKSEL = 0b000;
-    CCP1CON1bits.TMRPS = 0b00;
-    CCP1CON1bits.TRIGEN = 0;
-    CCP1CON1bits.SYNC = 0b00000;
-    
-    // Configure MCCP output for PWM signal
-    CCP1CON2bits.OCAEN = 1;
-    CCP1CON3bits.OUTM = 0b000;
-    CCP1CON3bits.POLACE = 0;
-    CCP1TMRbits.TMRL = 0x0000;
-    
-    // SN: 2017-00076
-    // Freq: 20kHz
-    // Duty: 76%
-    CCP1PRbits.PRL = 400;               // 8MHz / 20kHz
-    CCP1RA = 0;                 // edge-aligned PWM
-    CCP1RB = 304;               // 400 * 0.76
-        
-    CCP1CON1bits.ON = 1;
 }
 
 //void setup_oc() {
 //    // Set SCCP operating mode
-//    CCP2CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
-//    CCP2CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
+//    CCP1CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
+//    CCP1CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
 //    
 //    // Configure SCCP Timebase
-//    CCP2CON1bits.T32 = 0;
-//    CCP2CON1bits.TMRSYNC = 0;
-//    CCP2CON1bits.CLKSEL = 0b000;
-//    CCP2CON1bits.TMRPS = 0b00;
-//    CCP2CON1bits.TRIGEN = 0;
-//    CCP2CON1bits.SYNC = 0b00000;
+//    CCP1CON1bits.T32 = 0;
+//    CCP1CON1bits.TMRSYNC = 0;
+//    CCP1CON1bits.CLKSEL = 0b000;
+//    CCP1CON1bits.TMRPS = 0b00;
+//    CCP1CON1bits.TRIGEN = 0;
+//    CCP1CON1bits.SYNC = 0b00000;
 //    
 //    // Configure MCCP output for PWM signal
-//    CCP2CON2bits.OCAEN = 1;
-//    CCP2CON3bits.POLACE = 0;
-//    CCP2TMRbits.TMRL = 0x0000;
+//    CCP1CON2bits.OCAEN = 1;
+//    CCP1CON3bits.OUTM = 0b000;
+//    CCP1CON3bits.POLACE = 0;
+//    CCP1TMRbits.TMRL = 0x0000;
 //    
 //    // SN: 2017-00076
 //    // Freq: 20kHz
 //    // Duty: 76%
-//    CCP2PR = 400;               // 8MHz / 20kHz
-//    CCP2RA = 0;                 // edge-aligned PWM
-//    CCP2RB = 304;               // 400 * 0.76
+//    CCP1PRbits.PRL = 400;               // 8MHz / 20kHz
+//    CCP1RA = 0;                 // edge-aligned PWM
+//    CCP1RB = 304;               // 400 * 0.76
 //        
-//    CCP2CON1bits.ON = 1;
+//    CCP1CON1bits.ON = 1;
 //}
+
+void setup_oc() {
+    // Set SCCP operating mode
+    CCP2CON1bits.CCSEL = 0;     // Set SCCP operating mode (OC mode)
+    CCP2CON1bits.MOD = 0b0101;  // Set mode (Buffered Dual-Compare/PWM mode)
+    
+    // Configure SCCP Timebase
+    CCP2CON1bits.T32 = 0;
+    CCP2CON1bits.TMRSYNC = 1;
+    CCP2CON1bits.CLKSEL = 0b000;
+    CCP2CON1bits.TMRPS = 0b00;
+    CCP2CON1bits.TRIGEN = 0;
+    CCP2CON1bits.SYNC = 0b00000;
+    
+    // Configure SCCP output for PWM signal
+    CCP2CON2bits.OCAEN = 1;
+    CCP2CON3bits.POLACE = 1;
+    CCP2TMRbits.TMRL = 0x0000;
+    
+    // SN: 2017-00076
+    // Freq: 20kHz
+    // Duty: 76%
+    CCP2PR = 400;               // 8MHz / 20kHz
+    CCP2RA = 0;                 // edge-aligned PWM
+    CCP2RB = 304;               // 400 * 0.76
+        
+    CCP2CON1bits.ON = 1;
+}
 
